b30bc8a21be627cdd9da59ef89378d79407fe7dc
Thomas Ardal <thomasardal@gmail.com>
2023-01-16 14:12:40 +0100

    Initial commit

---
+++ CronExpressions/CronExpressionQuickInfoSourceProvider.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace CronExpressions
+{
+    [Export(typeof(IAsyncQuickInfoSourceProvider))]
+    [Name("Quick info when hovering cron expressions")]
+    [Order(After = "default")]
+    [ContentType("CSharp")]
+    public class CronExpressions : IAsyncQuickInfoSourceProvider
+    {
+        public IAsyncQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
+        {
+            return new CronExpressionQuickInfoSource(textBuffer);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Cron Expressions
+================
+
+Quick info when hovering cron expressions in Visual Studio:
+
+![Cron Expressions](screenshot.png)
+
+This extension is sponsored by [elmah.io](https://elmah.io).
