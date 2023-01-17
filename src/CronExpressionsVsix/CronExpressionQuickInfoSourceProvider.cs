7f6544e75bdb5fcd6b324b606b755ab6c251ba5e
Thomas Ardal <thomasardal@gmail.com>
2023-01-17 09:44:11 +0100

    Renamed vsix project

--- src/CronExpressionsVsix/CronExpressionQuickInfoSourceProvider.cs
+++ src/CronExpressionsVsix/CronExpressionQuickInfoSourceProvider.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.VisualStudio.Language.Intellisense;
+﻿using CronExpressions;
+using Microsoft.VisualStudio.Language.Intellisense;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Utilities;
 using System.ComponentModel.Composition;
 
-namespace CronExpressions.Vsix
+namespace CronExpressionsVsix
 {
     [Export(typeof(IAsyncQuickInfoSourceProvider))]
     [Name("Quick info when hovering cron expressions")]
