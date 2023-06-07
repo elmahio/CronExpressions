366c6f7705349f21526dc55259d6906bcef4a439
Thomas Ardal <thomasardal@gmail.com>
2023-06-07 12:11:03 +0200

    Moved Cronos as internal code since analyzer project doesn't seem to allow references all nuget packages

---
+++ src/CronExpressions.Analyzers/External/Cronos/DateTimeHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cronos
+{
+    internal static class DateTimeHelper
+    {
+        private static readonly TimeSpan OneSecond = TimeSpan.FromSeconds(1);
+
+        public static DateTimeOffset FloorToSeconds(DateTimeOffset dateTimeOffset) => dateTimeOffset.AddTicks(-GetExtraTicks(dateTimeOffset.Ticks));
+
+        public static bool IsRound(DateTimeOffset dateTimeOffset) => GetExtraTicks(dateTimeOffset.Ticks) == 0;
+
+        private static long GetExtraTicks(long ticks) => ticks % OneSecond.Ticks;
+    }
+}
