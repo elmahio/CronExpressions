d8ff9d57608fd0e2a05935ce30f1f105032c0882
Thomas Ardal <thomasardal@gmail.com>
2023-07-02 09:19:42 +0200

    Better analyzer for deciding when to show the tohuman result

--- src/CronExpressions.Analyzers/HumanTextToCronAnalyzer.cs
+++ src/CronExpressions.Analyzers/HumanTextToCronAnalyzer.cs
@@ -3,6 +3,8 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.CSharp;
 using System.Collections.Immutable;
+using System.Linq;
+using System;
 
 namespace CronExpressions.Analyers
 {
@@ -25,19 +27,35 @@ namespace CronExpressions.Analyers
             {
                 var stringLiteralExpr = (LiteralExpressionSyntax)ctx.Node;
                 var str = stringLiteralExpr.Token.ValueText;
-                if (string.IsNullOrWhiteSpace(str)) return;
-                str = str.TrimStart('\"').TrimEnd('\"').ToLower();
-                if (string.IsNullOrWhiteSpace(str)) return;
-
-                if (str.Contains("every ")
-                    || str.Contains("at ")
-                    || str.Contains("once")
-                    || str.Contains("each "))
+                if (ShouldReport(str))
                 {
                     var diagnostic = Diagnostic.Create(Rule, stringLiteralExpr.GetLocation());
                     ctx.ReportDiagnostic(diagnostic);
                 }
+
             }, SyntaxKind.StringLiteralExpression);
+        }
+
+        public static bool ShouldReport(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) return false;
+            str = str.TrimStart('\"').TrimEnd('\"').ToLower();
+            if (string.IsNullOrWhiteSpace(str)) return false;
+
+            var terms = str.ToLowerInvariant().Split(' ');
+            if (terms.Contains("once")) return true;
+            else
+            {
+                for (var i = 0; i < terms.Length; i++)
+                {
+                    var term = terms[i];
+                    if (term == "every" && terms.Length > 1+i) return true;
+                    else if (term == "at" && terms.Length > 1+i) return true;
+                    else if (term == "each" && terms.Length > 1+i) return true;
+                }
+            }
+
+            return false;
         }
     }
 }
---
+++ test/CronExpressions.Test/HumanTextToCronAnalyzerTest.cs
@@ -0,0 +1,23 @@
+﻿using CronExpressions.Analyers;
+using NUnit.Framework;
+
+namespace CronExpressions.Test
+{
+    public class HumanTextToCronAnalyzerTest
+    {
+        [TestCase("once", true)]
+        [TestCase("every hour", true)]
+        [TestCase("every", false)]
+        [TestCase("hour every", false)]
+        [TestCase("at 12 pm", true)]
+        [TestCase("at 12", true)]
+        [TestCase("at", false)]
+        [TestCase("about at", false)]
+        [TestCase("each hour", true)]
+        [TestCase("each", false)]
+        [TestCase("about each", false)]
+        [TestCase("", false)]
+        [TestCase("reach ", false)] // https://github.com/elmahio/CronExpressions/issues/4
+        public void CanShouldReport(string input, bool expected) => Assert.That(HumanTextToCronAnalyzer.ShouldReport(input), Is.EqualTo(expected));
+    }
+}
